d87c5372d5e0689a76ef12350245b3e59b6f7b53
justice chimobi <chimobi.justice@gmail.com>
2021-04-10 17:37:13 -0600

    fix-pathner

---
+++ pathner.php
@@ -0,0 +1,20 @@
+<section id="pathners" class="pt-5 pb-3">
+    <div class="container text-center">
+        <h6 class="text-primary">PATHNER</h6>
+        <h3>Our Trusted Pathners</h3>
+        <div class="pathners_images">
+            <div>
+                <img src="assets/images/twilio.svg" alt="">
+                <img src="assets/images/ibm.svg" alt="">
+            </div>
+            <div>
+                <img src="assets/images/google.webp" alt="">
+                <img src="assets/images/slack.svg" alt="">
+            </div>
+        </div>
+        <div>
+            <h5 class="text-info mt-5">TTPC PAID LLC</h5>
+            <p>Privacy Policy | Terms Of Services</p>
+        <div>
+    </div>
+</section>
