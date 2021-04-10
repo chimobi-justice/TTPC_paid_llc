561a0ac1e997f5ec37f42129519e49362ec2116f
justice chimobi <chimobi.justice@gmail.com>
2021-04-10 17:24:31 -0600

    hero-section

---
+++ herosection.php
@@ -0,0 +1,13 @@
+<section id="hero_section" class="container-fluid p-3 pb-5 pt-5">
+    <div class="container" id="hero_wrapper">
+        <div class="hero_content p-2">
+            <h1>Looking For A Modern Prepaid Program Manager? Just Ask</h1>
+            <p>Finding a modern prepaid program manager that is as responsive, agile and service-oriented as you are is harder than ever...until now. Want to make it easy? Just Ask TTPC</p>
+            <button class="btn btn-info btn-lg m-2">Just Ask!</button>
+            <button class="btn btn-default btn-lg c-my-btn m-2">Learn More</button>
+        </div>
+        <div class="hero_man">
+            <img src="assets/images/a1623fa8b83578b5ba9b86897308246a.png" alt="hero_man TTPC paid llc">
+        </div>
+    </div>
+</section>
