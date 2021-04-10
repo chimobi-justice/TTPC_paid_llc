3db86e36165b583b8196d9af68990b63fd407ca2
justice chimobi <chimobi.justice@gmail.com>
2021-04-10 17:31:45 -0600

    fix-product-section

---
+++ product.php
@@ -0,0 +1,48 @@
+<section id="product" class="pt-5 pb-5">
+    <div class="container product_wrap_holder d-flex justify-content-around">
+        <div class="product_content">
+            <h6 class="text-primary">PRODUCTS</h6>
+            <h3>Best Products to Facilitate Your Transaction</h3>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using is that is has a more-or-less normal distribution.</p>
+            <button class="btn btn-info btn-lg productsBtn">View More Products</button>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="products_cards">
+                    <div>
+                        <img src="assets/images/logos.png" alt="">
+                        <h6>TTPC cards programs</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="products_cards">
+                    <div>
+                        <img src="assets/images/logos.png" alt="">
+                        <h6>Reward/Incentive Card</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+             <div class="col-md-6">
+                <div class="products_cards">
+                    <div>
+                    <img src="assets/images/logos.png" alt="">
+                    <h6>Purchasing Card</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="products_cards">
+                    <div>
+                        <img src="assets/images/logos.png" alt="">
+                        <h6>Limited/Restricted use Card</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
