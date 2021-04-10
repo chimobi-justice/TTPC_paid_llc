a9bc2883d63f135569453a111c516a566b18fb1b
justice chimobi <chimobi.justice@gmail.com>
2021-04-10 16:52:23 -0600

    inc

---
+++ inc/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/styles/header.css">
+    <link rel="stylesheet" href="assets/styles/herosection.css">
+    <link rel="stylesheet" href="assets/styles/product.css">
+    <link rel="stylesheet" href="assets/styles/services.css">
+    <link rel="stylesheet" href="assets/styles/tech_section.css">
+    <link rel="stylesheet" href="assets/styles/pathners.css">
+    <link rel="stylesheet" href="assets/styles/footer.css">
+    <title>TTPC paid llc</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white" id="navbar">
+      <h1><a class="navbar-brand brand" href="#"><i>TTPC paid llc</i></a></h1>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#navbar">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#product">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#tech_section">Technology</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#pathners">Pathners</a>
+          </li>
+        </ul>
+        <div>
+        <form class="form-inline my-2 my-lg-0">
+            <a  href="auth/login.php" class="text-dark m-2" id="signIn">Sign in</a>  
+            <a  href="auth/register.php" class="btn c-my-btn btn-md text-primary" id="signupBtn">Register</a>
+        </form>
+        </div>
+      </div>
+  </nav>
+    
